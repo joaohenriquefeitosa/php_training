253a697aa262d7934d84dc3da319d301d15fc924
Joao Henrique <joaohenriquefsf@gmail.com>
2022-12-23 01:10:34 -0300

    working with array_pop

---
+++ php_arrays/array_pop/example01-fruits.php
@@ -0,0 +1,12 @@
+<?php
+
+// Example 01 - Fruits
+$fruits = [
+    'banana',
+    'apple',
+    'avocado'
+];
+
+$element = array_pop($fruits);
+var_dump($element);
+var_dump($fruits);
---
+++ php_arrays/array_pop/readme.md
@@ -0,0 +1,8 @@
+# Funtion array_pop
+
+## Description
+```
+Pop the element off the end of array and return the element.
+
+array_pop(array &$array): mixed
+```
