45527b273d8dbbf68b226e900fc03e192303f970
Joao Henrique <joaohenriquefsf@gmail.com>
2022-12-23 01:17:30 -0300

    working with array_push

---
+++ php_arrays/array_push/example01-fruits.php
@@ -0,0 +1,14 @@
+<?php
+
+// Example 01 - Fruits
+$fruits = [
+    'banana',
+    'apple',
+    'avocado'
+];
+
+array_push($fruits, 'guava');
+var_dump($fruits);
+
+array_push($fruits, 'pineapple', 'pear');
+var_dump($fruits);
---
+++ php_arrays/array_push/readme.md
@@ -0,0 +1,8 @@
+# Funtion array_push
+
+## Description
+```
+Push one or more elements onto the end of array
+
+array_push(array &$array, midex ...$values): int
+```
