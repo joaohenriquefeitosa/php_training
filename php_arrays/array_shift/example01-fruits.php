c9ce7e474a74b110303d21e670f4f7894edd11c2
Joao Henrique <joaohenriquefsf@gmail.com>
2023-01-16 00:36:58 -0300

    working with array_shift

---
+++ php_arrays/array_shift/example01-fruits.php
@@ -0,0 +1,8 @@
+<?php
+
+$stack = array('orange', 'banana', 'apple', 'raspberry');
+
+$fruit = array_shift($stack);
+
+var_dump($fruit);
+var_dump($stack);
---
+++ php_arrays/array_shift/readme.md
@@ -0,0 +1,8 @@
+# Funtion array_shift
+
+## Description
+```
+Shift an element off the beginning of array.
+
+array_shift(array $array): mixed
+```
